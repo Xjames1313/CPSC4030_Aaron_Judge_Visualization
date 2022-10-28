c667fe8eb3b52e4c5a9ee1f56352a30c82390591
Xjames133 <xjames@clemson.edu>
2022-10-27 20:35:58 -0400

    Initial commit

---
+++ script-AreaFreq.js
@@ -0,0 +1,13 @@
+d3.csv("stats.csv").then(
+    function(data){
+
+        var width = 500
+        var height = 500
+
+        var svg = d3.select("HR Area Frequency")
+                    .style("width", width)
+                    .style("height", height)
+
+        
+    }
+)
---
+++ script-GeoHR.js
@@ -0,0 +1,13 @@
+d3.csv("stats.csv").then(
+    function(data){
+
+        var width = 500
+        var height = 500
+
+        var svg = d3.select("Geographic HR Hits")
+                    .style("width", width)
+                    .style("height", height)
+
+        
+    }
+)
