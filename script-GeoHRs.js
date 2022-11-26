2bdd3487ee87229082bec6f4053434978efe7a08
jrleech <87986926+jrleech@users.noreply.github.com>
2022-11-26 15:01:16 -0600

    Add files via upload

---
+++ script-GeoHRs.js
@@ -0,0 +1,71 @@
+d3.json("homerunpositions.json").then(
+  function (dataset) {
+
+    var dimensions = {
+      width: 1000,
+      height: 600,
+      margin: {
+        top: 10,
+        bottom: 50,
+        right: 10,
+        left: 50
+      }
+    }
+
+    console.log(dataset)
+
+    var xAccessor = d => d.Position
+    var yAccessor = d => d.Dist
+
+    var svg = d3.select("#GeoHRs")
+      .style("width", dimensions.width)
+      .style("height", dimensions.height)
+
+
+    var xScale = d3.scaleLinear()
+      .domain(d3.extent(dataset, xAccessor))
+      .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
+
+    var yScale = d3.scaleLinear()
+      .domain(d3.extent(dataset, yAccessor))
+      .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
+
+    var dots = svg.append("g")
+      .selectAll("circle")
+      .data(dataset)
+      .enter()
+      .append("circle")
+
+      .on("click", function () {
+        d3.select(this)
+          .attr("stroke-width", "2")
+          .attr("stroke", "red")
+      })
+      .attr("cx", 0)
+      .attr("cy", 0)
+      .attr("r", 3)
+      .attr("fill", "black")
+
+    d3.select("#start")
+      .on("click", function () {
+        dots
+
+          .transition().duration(1000)
+          .attr("cx", d => xScale(xAccessor(d)))
+          .attr("cy", d => yScale(yAccessor(d)))
+          .attr("r", 3)
+          .attr("fill", "black")
+      })
+
+    var xAxisGen = d3.axisBottom().scale(xScale)
+    var xAxis = svg.append("g")
+      .call(xAxisGen)
+      .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`)
+
+    var yAxisGen = d3.axisLeft().scale(yScale)
+    var yAxis = svg.append("g")
+      .call(yAxisGen)
+      .style("transform", `translateX(${dimensions.margin.left}px)`)
+
+  }
+)     
